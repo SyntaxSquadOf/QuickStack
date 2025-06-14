e8f384b7eddfdf648a25ec5d516f7a4f540040f7
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-06-14 01:20:33 -0500

    feat: update TypeScript configuration, add logger utility, and refine development scripts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import colors from "colors";
 import app from "./server";
-import { PORT } from "./config/process";
+import { PORT } from "./config/Process";
 
 app.listen(PORT, () => {
   console.log(colors.blue.bold(`Server running on port ${PORT}`));
