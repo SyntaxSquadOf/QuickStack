30d2b67be7161e6977a5a731c1dfcd38b9e4f2e1
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-07-23 14:29:21 -0500

    feat: add helmet middleware for enhanced security

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import { RateLimit } from "./utils/rateLimit";
+import helmet from "helmet";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(RateLimit);
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
