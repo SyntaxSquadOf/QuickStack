afe0689f6eb8b9ba1b97e60b85fc0952588f6148
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-07-23 14:35:10 -0500

    fix: use middleware morgan and add comments to usage for better clarity

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, { Express } from "express";
 import { RateLimit } from "./utils/rateLimit";
 import helmet from "helmet";
+import morgan from "morgan";
 
 const app: Express = express();
 
-app.use(express.json());
-app.use(RateLimit);
-app.use(helmet());
+app.use(express.json()); // Middleware para parsear el cuerpo de las solicitudes JSON
+app.use(RateLimit); // Middleware para limitar la tasa de solicitudes
+app.use(helmet()); // Middleware para mejorar la seguridad de la aplicación
+app.use(morgan("dev")); // Middleware para registrar las solicitudes HTTP
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
